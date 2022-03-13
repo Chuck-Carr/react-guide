611bc2ed268cd33f9d78d0152615761fcbeb4d3b
Chuck <chuck.carr4@yahoo.com>
2022-03-13 10:47:45 -0400

    added an alert box if any input fields were emtpy while adding a new expense

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -5,51 +5,46 @@ const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
-  //   const [userInput, setUserInput] = useState({
-  //     enteredTitle: "",
-  //     enteredAmount: "",
-  //     enteredDate: "",
-  //   });
 
   const titleChangeHandler = (event) => {
     setEnteredTitle(event.target.value);
-    // setUserInput((prevState) => {
-    //   return { ...prevState, enteredTitle: event.target.value };
-    // });
   };
   const amountChangeHandler = (event) => {
     setEnteredAmount(event.target.value);
-    // setUserInput((prevState) => {
-    //     return { ...prevState, enteredAmount: event.target.value };
-    //   });
   };
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
-    // setUserInput((prevState) => {
-    //     return { ...prevState, enteredDate: event.target.value };
-    //   });   
   };
   const submitHander = (event) => {
     event.preventDefault();
-
-    const expenseData = {
+    if (
+      enteredTitle.trim() === "" ||
+      enteredAmount.trim() === "" ||
+      enteredDate.trim() === ""
+    ) {
+      alert("Please enter all fields for adding an expense.");
+    } else {
+      const expenseData = {
         title: enteredTitle,
         amount: enteredAmount,
         date: new Date(enteredDate),
+      };
+      props.onSaveExpenseData(expenseData);
+      setEnteredTitle("");
+      setEnteredAmount("");
+      setEnteredDate("");
     }
-
-    props.onSaveExpenseData(expenseData);
-    setEnteredTitle('');
-    setEnteredAmount('');
-    setEnteredDate('');
-
-  }
+  };
   return (
     <form onSubmit={submitHander}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
